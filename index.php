0a78ead8aa85123019348c93a623f00e1c7d349e
Herik Martins <herikcosmo.m@gmail.com>
2021-09-29 16:40:03 -0300

    enviar mensagem para whatsapp

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
                 <th>Nome</th>
                 <th>Email</th>
                 <th>Telefone</th>
+                <th>ZAP</th>
                 <th>Editar</th>
                 <th>Excluir</th>
             </tr>
@@ -38,6 +39,7 @@
                     <th><?php echo $contato['nome'] ?></th>
                     <th><?php echo $contato['email'] ?></th>
                     <th><?php echo $contato['telefone'] ?></th>
+                    <th><a href="https://api.whatsapp.com/send?phone=55<?php echo filter_var(str_replace('-', '', $contato['telefone']), FILTER_SANITIZE_NUMBER_INT) ?>" target='_blank'>Conversar</a></th>
                     <th><a href="edicao_contato.php?id=<?php echo $contato['id']?>">Editar</a></th>
                     <th><a href="excluir_contato.php?id=<?php echo $contato['id']?>">Excluir</a></th>
                 </tr>
