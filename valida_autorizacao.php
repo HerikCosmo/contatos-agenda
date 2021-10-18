2a4ba6ea0708e66fd3d946168770768bcfff2b4f
Herik Cosmo <herikcosmo.m@gmail.com>
2021-10-18 09:25:32 -0300

    Update valida_autorizacao.php

--- valida_autorizacao.php
+++ valida_autorizacao.php
@@ -6,4 +6,5 @@ if(!isset($_SESSION['login']) || empty($_SESSION['login']) ||
     !isset($_SESSION['nome']) || empty($_SESSION['nome']) || 
     !isset($_SESSION['id']) || empty($_SESSION['id'])) {
     header('Location: login.php');
+    exit;
 }
